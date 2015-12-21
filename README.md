ae2f2eb500092599af8d3d26d16107657a469e92
Kier Davis <kierdavis@gmail.com>
2015-12-21 19:17:56 +0000

    Initial import

---
+++ README.md
@@ -0,0 +1,94 @@
+# Hardware
+
+The LCD module has 16 pins; from left to right they are:
+
+Pin  Signal         Type     Description
+---  -------------  -------  -----------
+1    `VSS`          Power    Ground
+2    `VDD`          Power    Logic power supply
+3    `V0`           Power    LCD power supply
+4    `RS`           Control  Register select (low = instruction, high = data)
+5    `RW`           Control  Read/write select (low = write, high = read)
+6    `E`            Control  Enable
+7    `DB0`          Data     Data bus (bit 0)
+8    `DB1`          Data     Data bus (bit 1)
+9    `DB2`          Data     Data bus (bit 2)
+10   `DB3`          Data     Data bus (bit 3)
+11   `DB4`          Data     Data bus (bit 4)
+12   `DB5`          Data     Data bus (bit 5)
+13   `DB6`          Data     Data bus (bit 6)
+14   `DB7`          Data     Data bus (bit 7)
+15   `LED+` or `A`  Power    Backlight power supply
+16   `LED-` or `K`  Power    Backlight ground
+
+## Wiring up the power supply
+
+First, connect `VSS` (pin 1) to ground (0V) and `VDD` (pin 2) to a +5V power supply.
+
+The LCD power supply voltage can be varied to adjust the contrast. A potentiometer can be used to allow adjusting this, but here we will just make a simple voltage divider out of two resistors.
+
+Grab two resistors of the same value (anything between about 10 kΩ and 100 kΩ will do). Connect one between the +5V supply and `V0` (pin 3), and the other between `V0` and ground. This gives `V0` a voltage of 2.5V, which is good enough for now. You can always swap the resistors for different values to change the voltage of `V0`.
+
+Lastly, connect up the backlight power by wiring `LED-` (pin 16) to ground and adding a 220 Ω resistor between `LED+` (pin 15) and the +5V supply.
+
+## Wiring up the control signals
+
+It is unusual to need to read data from the LCD. Mostly you'll just want to write data (text to display, cursor position etc.) to it, so we'll just wire the read/write select pin `RW` to ground, indicating that it will always be in write mode.
+
+The other two control signals, `RS` and `E`, will need to be connected to the GPIO pins on the Raspberry Pi. 
+
+Control pin mapping:
+
+LCD signal    Raspberry Pi signal
+------------  -------------------
+`RS` (pin 4)  GPIO pin 4
+`E` (pin 6)   GPIO pin 27
+
+## Wiring up the data signals
+
+The LCD provides both 8-bit and 4-bit modes. We will use the 4-bit mode, so only pins `DB4`, `DB5`, `DB6`, `DB7` are actually used for communication. Pins `DB0`, `DB1`, `DB2` and `DB3` are left disconnected.
+
+LCD signal      Raspberry Pi signal
+--------------  -------------------
+`DB4` (pin 11)  GPIO pin 22
+`DB5` (pin 12)  GPIO pin 23
+`DB6` (pin 13)  GPIO pin 24
+`DB7` (pin 14)  GPIO pin 25
+
+
+# Software
+
+`lcd1602.py` contains a class that can be used for controlling the LCD display.
+
+Example usage:
+
+    # Create LCD controller, specifying the pin numbers.
+    # This also initialises the controller and clears the screen.
+    lcd = lcd1602.LCD(4, 27, 22, 23, 24, 25)
+    
+    # Write some text.
+    lcd.write("Hello, world!")
+    
+    # Clear the screen.
+    lcd.clear()
+    
+    # Move the cursor to the top left.
+    lcd.home()
+    
+    # Set cursor coordinates to second line, fourth character.
+    lcd.set_cursor(1, 3)
+    
+    # Move cursor one character to the right.
+    lcd.move_cursor(lcd1602.RIGHT)
+    
+    # Shift display one character to the left.
+    lcd.shift_display(lcd1602.LEFT)
+    
+    # Change settings. See the docstrings for the set_entry_mode and
+    # set_display_mode methods for information on what the parameters mean.
+    lcd.set_entry_mode(direction    = lcd1602.RIGHT,
+                       shift_enable = False)
+    
+    lcd.set_display_mode(display_on     = True,
+                         cursor_visible = False,
+                         cursor_blink   = False)
---
+++ lcd1602.py
@@ -0,0 +1,300 @@
+# 1602 LCD module controller for the Raspberry Pi
+
+import time
+import RPi.GPIO as GPIO
+
+LEFT = object()
+RIGHT = object()
+
+_ENTRY_MODE_DIRECTION = 1
+_ENTRY_MODE_SHIFT_ENABLE = 0
+
+_DISPLAY_MODE_DISPLAY_ON = 2
+_DISPLAY_MODE_CURSOR_VISIBLE = 1
+_DISPLAY_MODE_CURSOR_BLINK = 0
+
+class Deadline(object):
+    def __init__(self):
+        self.t = time.clock()
+    
+    def set(self, secs_from_now):
+        self.t = time.clock() + secs_from_now
+    
+    def wait(self):
+        if time.clock() - self.t > 500e-6:
+            time.sleep(time.clock() - self.t)
+        
+        while time.clock() < self.t:
+            pass
+
+class LCD(object):
+    def __init__(self, rs_pin, e_pin, db4_pin, db5_pin, db6_pin, db7_pin):
+        self.rs_pin = rs_pin
+        self.e_pin = e_pin
+        self.db4_pin = db4_pin
+        self.db5_pin = db5_pin
+        self.db6_pin = db6_pin
+        self.db7_pin = db7_pin
+        
+        # Used to ensure that we do not send another command before the previous
+        # one has finished.
+        self.deadline = Deadline()
+        
+        self._entry_mode = 0
+        self._display_mode = 0
+        
+        self._init_pins()
+        self._init_lcd()
+    
+    def _init_pins(self):
+        """
+        Set all pins as outputs and initialise them to their idle states.
+        """
+        
+        GPIO.setup(self.rs_pin, GPIO.OUT)
+        GPIO.setup(self.e_pin, GPIO.OUT)
+        GPIO.setup(self.db4_pin, GPIO.OUT)
+        GPIO.setup(self.db5_pin, GPIO.OUT)
+        GPIO.setup(self.db6_pin, GPIO.OUT)
+        GPIO.setup(self.db7_pin, GPIO.OUT)
+        GPIO.output(self.rs_pin, False)
+        GPIO.output(self.e_pin, False)
+        GPIO.output(self.db4_pin, False)
+        GPIO.output(self.db5_pin, False)
+        GPIO.output(self.db6_pin, False)
+        GPIO.output(self.db7_pin, False)
+    
+    def _init_lcd(self):
+        """
+        Initialise the LCD.
+        """
+        
+        # LCD needs 40 ms to start up after power on
+        self.deadline.set(40e-3)
+        
+        # Synchronise and set to 4 bit mode
+        self._send_4bit(0x3)
+        self.deadline.set(4.5e-3) # 4.5 ms
+        self._send_4bit(0x3)
+        self.deadline.set(150e-6) # 150 us
+        self._send_4bit(0x3)
+        self.deadline.set(150e-6) # 150 us
+        self._send_4bit(0x2)
+        self.deadline.set(150e-6) # 150 us
+        
+        # Set function (4-bit mode, 2 lines, 5x8 characters)
+        self._send_command(0x28)
+        self.deadline.set(37e-6)
+        
+        # Set parameters
+        self.set_display_mode(display_on = False, cursor_visible = False, cursor_blink = False)
+        self.clear()
+        self.set_entry_mode(direction = RIGHT, shift_enable = False)
+        self.set_display_mode(display_on = True)
+    
+    def clear(self):
+        """
+        Clear the display.
+        """
+        
+        self._send_command(0x01)
+        self.deadline.set(1.52e-3)
+    
+    def home(self):
+        """
+        Move the cursor to the home position (top left corner).
+        """
+        
+        self._send_command(0x02)
+        self.deadline.set(1.52e-3)
+    
+    def set_entry_mode(self, direction = None, shift_enable = None):
+        """
+        Set entry mode settings.
+        
+        direction:
+            The direction in which the cursor will move after each character is
+            written (either LEFT or RIGHT).
+        shift_enable:
+            Whether or not the entire display will shift when a character is
+            written. The cursor moves with the display.
+        
+        A value of None can also be given as a settings, which indicates that
+        the setting should retain its previous value. All parameters default to
+        None.
+        """
+        
+        if direction is not None:
+            if direction is RIGHT:
+                self._entry_mode |= (1 << _ENTRY_MODE_DIRECTION)
+            elif direction is LEFT:
+                self._entry_mode &= ~(1 << _ENTRY_MODE_DIRECTION)
+            else:
+                raise ValueError("Expected either LEFT or RIGHT for parameter 'direction'")
+        
+        if shift_enable is not None:
+            if shift_enable:
+                self._entry_mode |= (1 << _ENTRY_MODE_SHIFT_ENABLE)
+            else:
+                self._entry_mode &= ~(1 << _ENTRY_MODE_SHIFT_ENABLE)
+        
+        self._send_command(0x04 | (self._entry_mode & 0x03))
+        self.deadline.set(37e-6)
+    
+    def set_display_mode(self, display_on = None, cursor_visible = None, cursor_blink = None):
+        """
+        Set display mode settings.
+        
+        display_on;
+            Whether or not the display is enabled.
+        cursor_visible:
+            Whether or not the cursor is visible.
+        cursor_blink:
+            Whether or not cursor blinking is enabled.
+        
+        A value of None can also be given as a settings, which indicates that
+        the setting should retain its previous value. All parameters default to
+        None.
+        """
+        
+        if display_on is not None:
+            if display_on:
+                self._display_mode |= (1 << _DISPLAY_MODE_DISPLAY_ON)
+            else:
+                self._display_mode &= ~(1 << _DISPLAY_MODE_DISPLAY_ON)
+        
+        if cursor_visible is not None:
+            if cursor_visible:
+                self._display_mode |= (1 << _DISPLAY_MODE_CURSOR_VISIBLE)
+            else:
+                self._display_mode &= ~(1 << _DISPLAY_MODE_CURSOR_VISIBLE)
+        
+        if cursor_blink is not None:
+            if cursor_blink:
+                self._display_mode |= (1 << _DISPLAY_MODE_CURSOR_BLINK)
+            else:
+                self._display_mode &= ~(1 << _DISPLAY_MODE_CURSOR_BLINK)
+        
+        self._send_command(0x08 | (self._display_mode & 0x07))
+        self.deadline.set(37e-6)
+    
+    def move_cursor(self, direction):
+        """
+        Move the cursor by one character in the given direction.
+        """
+        
+        if direction is LEFT:
+            self._send_command(0x10)
+        elif direction is RIGHT:
+            self._send_command(0x14)
+        else:
+            raise ValueError("Expected either LEFT or RIGHT for parameter 'direction'")
+        self.deadline.set(37e-6)
+    
+    def shift_display(self, direction):
+        """
+        Shift the displayed text by one character in the given direction.
+        """
+        
+        if direction is LEFT:
+            self._send_command(0x18)
+        elif direction is RIGHT:
+            self._send_command(0x1C)
+        else:
+            raise ValueError("Expected either LEFT or RIGHT for parameter 'direction'")
+        self.deadline.set(37e-6)
+    
+    def _set_cgram_addr(self, addr):
+        """
+        Set the LCD address counter to the given location in CGRAM.
+        """
+        
+        self._send_command(0x40 | (addr & 0x3F))
+        self.deadline.set(37e-6)
+    
+    def set_cursor(self, line, column):
+        """
+        Set the cursor and text insertion position. line and column are both
+        0-indexed.
+        """
+        
+        if line < 0 or line > 1::
+            raise ValueError("'line' out of range (0 to 1)")
+        if column < 0x00 or column > 0x39:
+            raise ValueError("'column' out of range (0 to 39)")
+        
+        self._set_ddram_addr(line*0x40 + column)
+    
+    def _set_ddram_addr(self, addr):
+        """
+        Set the LCD address counter to the given location in DDRAM.
+        """
+        
+        self._send_command(0x80 | (addr & 0x7F))
+        self.deadline.set(37e-6)
+    
+    def write(self, string):
+        """
+        Write text to the LCD at the current cursor position.
+        """
+        
+        if isinstance(string, unicode):
+            string = string.encode("ascii")
+        
+        for char in string:
+            self._write_byte(ord(value))
+    
+    def _write_byte(self, value):
+        """
+        Write a data byte to the current LCD RAM address, then increment or
+        decrement the RAM address according to the 'direction' entry mode
+        setting.
+        """
+        
+        self._send_data(value & 0xFF)
+        self.deadline.set(37e-6)
+    
+    def _send_command(self, value):
+        """
+        Send a command byte, setting the RS pin to indicate command mode.
+        """
+        
+        # Set RS low
+        GPIO.output(self.rs_pin, False)
+        
+        # MSB first
+        self._send_nibble(value >> 4, False)
+        self._send_nibble(value, False)
+    
+    def _send_data(self, value):
+        """
+        Send a data byte, setting the RS pin to indicate data mode.
+        """
+        
+        # Set RS high
+        GPIO.output(self.rs_pin, True)
+        
+        # MSB first
+        self._send_nibble(value >> 4, False)
+        self._send_nibble(value, False)
+    
+    def _send_nibble(self, value):
+        """
+        Send a nibble (4 bit value).
+        """
+        
+        # Do not start transmission until the LCD is done processing the last
+        # command.
+        self.deadline.wait()
+        
+        # Set data pins
+        GPIO.output(self.db4_pin, bool(value & 0x1))
+        GPIO.output(self.db5_pin, bool(value & 0x2))
+        GPIO.output(self.db6_pin, bool(value & 0x4))
+        GPIO.output(self.db7_pin, bool(value & 0x8)
+        
+        # Pulse enable pin
+        GPIO.output(self.e_pin, True)
+        _delay(1e-6) # 1 us
+        GPIO.output(self.e_pin, False)
+        _delay(1e-6) # 1 us
